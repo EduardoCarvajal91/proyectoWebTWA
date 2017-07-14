03a6ec5bb22b3c924acb87ee1668130fd74fab8f
EduardoCarvajal91 <edu.carvajal.t@gmail.com>
2017-07-14 12:40:09 -0400

    Database funcional y nueva clase Persona agregada.

---
+++ src/AppBundle/Entity/Persona.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Usuario
+ * Date: 14-07-2017
+ * Time: 12:37
+ */
+
+namespace AppBundle\Entity;
+
+class Persona
+{
+
+    private $nombre;
+    private $segundo_nombre;
+    private $apellido_paterno;
+    private $apellido_materno;
+    private $contrasena;
+
+
+
+}
